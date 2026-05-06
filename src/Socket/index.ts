6581423cf73be43a6815b304abc05d1770c16066
MuhammadIdsal <81858237+MuhammadIdsal@users.noreply.github.com>
2026-05-06 09:29:04 +0700

    Update log system

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -5,8 +5,11 @@ import { makeCommunitiesSocket } from './communities'
 // export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => {
 	const newConfig = {
-		...DEFAULT_CONNECTION_CONFIG,
-		...config
+	console.log('\x1b[36m%s\x1b[0m', '=========================================');
+  console.log('\x1b[32m%s\x1b[0m', '   🔥 BAILEYS CUSTOM BY IDSAL 🔥     ');
+  console.log('\x1b[33m%s\x1b[0m', '      STATUS: BOT SEDANG DIJALANKAN      ');
+  console.log('\x1b[36m%s\x1b[0m', '=========================================');
+	
 	}
 
 	return makeCommunitiesSocket(newConfig)
